e543fc83425d42a1b7e27733954fc4833c4298a2
Luiz Alberto <albertojiujitsu.alberto@gmail.com>
2024-05-23 19:08:28 -0300

    envio 4

--- ConsoleApp.ExerciciosMod01/Program.cs
+++ ConsoleApp.ExerciciosMod01/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp.ExerciciosMod01.Classes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        // List<ItemLocadora>;
+        Filme filme = new Filme(1, "Resgate Soldado Ryan", 1998, "Spilberg", 230);
+        filme.ExibirDetalhes();
+    }
+}
